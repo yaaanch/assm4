84cdf09245e604ae34d64cf5c35495b4c529d07e
Lorraine Neo <lorraineneo@gmail.com>
2014-08-03 07:05:16 +0000

    added new UserProfile class

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from bookmark.models import Category, Page
+from bookmark.models import Category, Page, UserProfile
 
 admin.site.register(Category)
 admin.site.register(Page)
+admin.site.register(UserProfile)
